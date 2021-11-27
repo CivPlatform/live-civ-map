7883125151aec6151c0f23d99881e4d919b3c0d2
Gjum <code.gjum@gmail.com>
2021-11-27 16:27:59 +0100

    default layer url configurable via env

--- web/src/map/Layer.ts
+++ web/src/map/Layer.ts
@@ -1,10 +1,13 @@
 import useLocalStorage from '@rehooks/local-storage'
 
+const DEFAULT_LAYER_URL =
+	process.env.REACT_APP_DEFAULT_LAYER_URL || 'ws://localhost:5000/dev'
+
 /** stored to remember known layers in the client's map */
 export type Layer = { url: string; hidden?: boolean }
 
 export function useLayers(): [Layer[], (newValue: Layer[]) => void] {
 	let [layers, setLayers] = useLocalStorage<Layer[]>('LiveCivMap:layers')
-	if (!layers) layers = []
+	if (!layers) layers = [{ url: DEFAULT_LAYER_URL }]
 	return [layers, setLayers]
 }
